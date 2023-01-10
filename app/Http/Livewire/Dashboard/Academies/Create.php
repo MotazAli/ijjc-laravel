4f09dd7ad7af145bee956824f4c9b9f9e1cdf785
Motaz Ali <motaz_ali@hotmail.com>
2023-01-10 03:06:28 +0200

    button behavior change

---
+++ app/Http/Livewire/Dashboard/Academies/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\Academies;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.academies.create');
+    }
+}
